a4a097956729b6074a416ec9cc2dcd1caff3dc78
chubinashvili <chubinashvili2003@gmail.com>
2022-11-25 16:27:12 +0400

    created logout hook

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -6,6 +6,8 @@ export const authReducer = (state, { type, payload }) => {
     switch (type) {    
         case 'LOGIN':
             return { ...state, user: payload };
+        case 'LOGOUT':
+            return { ...state, user: null };
         default:
             return state;
     }
---
+++ src/hooks/useLogout.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { projectAuth } from "../firebase/config";
+import { useAuthContext } from "./UseAuthContext";
+
+export const useLogout = () => {
+    const [error, setError] = useState(null);
+    const [isPending, setIsPending] = useState(false);
+    const { dispatch } = useAuthContext();
+
+    const logout = async () => {
+        setError(null)
+        setIsPending(true);
+
+        // sign the user out 
+        try {
+            await projectAuth.signOut();
+
+            // dispatch logout action
+            dispatch({ type: 'LOGOUT' });
+            setError(null);
+        } catch (err) {
+            console.log(err.message);
+            setError(err.message);
+        }
+        setIsPending(false);
+    }
+
+    return { useLogout, error, isPending };
+}
