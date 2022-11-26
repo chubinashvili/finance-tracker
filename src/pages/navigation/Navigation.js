5ef2d7839bc3ff4367e587281f51e39226006194
chubinashvili <chubinashvili2003@gmail.com>
2022-11-26 21:24:44 +0400

    added conditional on showing user content in navbar

--- src/pages/navigation/Navigation.js
+++ src/pages/navigation/Navigation.js
@@ -14,15 +14,20 @@ const Navigation = () => {
       <nav className={styles.navbar}>
           <ul>
               <li className={styles.title}>myMoney</li>
-              
-              <li><Link to="/login">Login</Link></li>
-              <li><Link to="/signup">Signup</Link></li> 
 
-              {user && (
+            {user ? (
+              <Fragment>
+                <li>hello, {user.displayName}</li>
                 <li>
                   <button className='btn' onClick={logout}>Logout</button>
                 </li>           
-              )}
+              </Fragment>
+            ) : (
+              <Fragment>
+                <li><Link to="/login">Login</Link></li>
+                <li><Link to="/signup">Signup</Link></li> 
+              </Fragment>              
+            )}
           </ul>
       </nav>
       <Outlet />
